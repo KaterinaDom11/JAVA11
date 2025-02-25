e7b6ffc14c0d502ac2df0dd83c7d3206fb3d2edc
Emma Paris <eparis@atlassian.com>
2025-02-25 16:07:51 +0300

    init afisa

---
+++ src/main/java/ru/netology/afisha/PosterManager.java
@@ -0,0 +1,44 @@
+package ru.netology.afisha;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PosterManager {
+    private String[] films = new String[0];
+    private int limit;
+
+    public PosterManager() {
+        this.limit = 5;
+    }
+
+    public PosterManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void save(String film) {
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        films = tmp;
+    }
+
+    public String[] findAll() {
+        return films;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (films.length < limit) {
+            resultLength = films.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] result = new String[resultLength];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = films[films.length - 1 - i];  // заполняем result из массива, что лежит в поле
+            // в обратном порядке
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/netology/afisha/PosterManagerTest.java
@@ -0,0 +1,72 @@
+package ru.netology.afisha;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PosterManagerTest {
+
+
+    @Test
+    public void creatingFirstMovie() {
+        PosterManager servis = new PosterManager();
+
+        servis.save("Бладшот");
+        servis.save("Вперед");
+        servis.save("Отель Белград");
+
+        String[] expected = {"Бладшот", "Вперед", "Отель Белград"};
+        String[] actual = servis.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void outputMinimumNumberOfFilms() {
+        PosterManager servis = new PosterManager();
+
+        servis.save("Бладшот");
+        servis.save("Вперед");
+        servis.save("Отель Белград");
+
+        String[] expected = {"Отель Белград", "Вперед", "Бладшот"};
+        String[] actual = servis.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void outputLimitNumberOfFilms() {
+        PosterManager servis = new PosterManager();
+
+        servis.save("Бладшот");
+        servis.save("Вперед");
+        servis.save("Отель Белград");
+        servis.save("Джентльмены");
+        servis.save("Человек-Невидимка");
+
+
+        String[] expected = {"Человек-Невидимка", "Джентльмены", "Отель Белград", "Вперед", "Бладшот"};
+        String[] actual = servis.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void outputMoreLimit() {
+        PosterManager servis = new PosterManager();
+
+        servis.save("Бладшот");
+        servis.save("Вперед");
+        servis.save("Отель Белград");
+        servis.save("Джентльмены");
+        servis.save("Человек-Невидимка");
+        servis.save("Тролли.Мировой тур");
+
+        String[] expected = {"Тролли.Мировой тур", "Человек-Невидимка", "Джентльмены", "Отель Белград", "Вперед"};
+        String[] actual = servis.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+}
